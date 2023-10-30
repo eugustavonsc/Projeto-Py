59ef435edcb8ba3d86fb322a2fd290d51ae9b758
Gusta <gustavo.slv1516@gmail.com>
2023-10-30 05:57:38 -0300

    comentarios

--- Projeto.py
+++ Projeto.py
@@ -1,12 +1,14 @@
 #criação de um menu para o usuario selecionar qual setor ele quer conferir
 def menu_opcao():
     op=int(input(" 1-Para cooperativa\n 2-Para Refeitorio\n 3-Para Agroindustria:\n"))
+    if (op!=int):
+        print("Digite um número valido!")
     return op
 
-def cooperativa():
+def cooperativa(): #menu de opções da coooperativa
     op=int(input(" 1-Ver estoque\n 2-Adicionar item\n 3-Remover item:\n"))
     if (op!=int):
-        print("Digite um número!")
+        print("Digite um número valido!")
     return op
 
 match menu_opcao():
