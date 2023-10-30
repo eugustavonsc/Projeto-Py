b6621a70f68598ae9f91336fe083a76ecc907724
Alan Sousa <alansousa456123@gmail.com>
2023-10-30 06:58:51 -0300

    alterações nas def's

--- Projeto.py
+++ Projeto.py
@@ -1,23 +1,44 @@
 #criação de um menu para o usuario selecionar qual setor ele quer conferir
 def menu_opcao():
   print(" Bem vindo(a) ao IF Administra! Qual estoque você deseja entrar?\n 1-Cooperativa\n 2-Refeitorio\n 3-Agroindustria:\n")
-  while True: #teste para o usuario não inserir nada alem de numeros
+  while True: #validar a opção do input
     try:
-      op = int(input("Digite um número: "))
+      op = int(input("Digite a opção desejada: "))
     except:
-      print("Você digitou uma letra. Por favor digite um número.")
+      print("Você digitou uma letra. Por favor digite apenas número.")
     else:
       return op
 
 def cooperativa(): #menu de opções da coooperativa
     op=int(input(" \nCooperativa! O que você deseja fazer?\n 1-Ver estoque\n 2-Adicionar item\n 3-Remover item\n 4-Voltar ao menu principal:\n"))
-    return op
+    while True:
+     try:
+        op = int(input("Digite a opção desejada: "))
+     except:
+        print("Você digitou uma letra. Por favor digite apenas número.")
+     else:
+        return op
 
 def refeitorio(): #menu de opções do refeitorio
-    op=int(input(" 1-Ver estoque\n 2-Adicionar item\n 3-Remover item:\n"))
-    if (op!=int):
-        print("Digite um número valido!")
-    return op
+    op=int(input(" \nRefeitório! O que você deseja fazer?\n 1-Ver estoque\n 2-Adicionar item\n 3-Remover item\n 4-Voltar ao menu principal:\n"))
+    while True:
+     try:
+        op = int(input("Digite a opção desejada: "))
+     except:
+        print("Você digitou uma letra. Por favor digite apenas número.")
+     else:
+        return op
+
+def agroindustria(): #menu de opções do agroindustria
+    op=int(input(" \nAgroindústria! O que você deseja fazer?\n 1-Ver estoque\n 2-Adicionar item\n 3-Remover item\n 4-Voltar ao menu principal:\n"))
+    while True:
+     try:
+        op = int(input("Digite a opção desejada: "))
+     except:
+        print("Você digitou uma letra. Por favor digite apenas número.")
+     else:
+        return op
+
 #Codigo
 match menu_opcao():
     case 1:
@@ -30,8 +51,6 @@ match menu_opcao():
                 matriz=[]
                 with open("cooperativa.txt","r") as arquivo: #leitura do arquivo e mostra o estoque detro dele
                     conteudo= arquivo.read()
-                    matriz= conteudo.split("\n")
-                    
 
                     
 
