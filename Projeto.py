afbeff7f829c04bfc9e1f9ea2ce3a8743fb9f0a5
Alan Sousa <alansousa456123@gmail.com>
2023-10-30 05:53:09 -0300

    alterações

--- Projeto.py
+++ Projeto.py
@@ -1,19 +1,18 @@
 #criação de um menu para o usuario selecionar qual setor ele quer conferir
-def menu_opcao(op):
+def menu_opcao():
     op=int(input(" 1-Para cooperativa\n 2-Para Refeitorio\n 3-Para Agroindustria:\n"))
     return op
-match menu_opcao():
-    case 1:
-        cooperativa()
-    case 2:
-    
-    case 3:
-    
-    case _:
-        print("comando invalido" )
-
 
 def cooperativa():
     op=int(input(" 1-Ver estoque\n 2-Adicionar item\n 3-Remover item:\n"))
+    if (op!=int):
+        print("Digite um número!")
     return op
 
+match menu_opcao():
+    case 1:
+        cooperativa()
+    case _:
+        print("comando invalido\n" )
+
+menu_opcao()
