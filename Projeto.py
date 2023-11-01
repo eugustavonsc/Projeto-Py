fc07f113dab41a58fe120c0734129d3e8fe9d23d
Gusta <gustavo.slv1516@gmail.com>
2023-11-01 05:13:26 -0300

    adição do while dentro do menu cooperativa

--- Projeto.py
+++ Projeto.py
@@ -43,46 +43,43 @@ def agroindustria(): #menu de opções do agroindustria
 #Obs: acho que se tirasse o menu_opção do match e deixasse só os outros menus, teria como deixar eles infinitos
 while True: #Deixar o programa rodando infinitamente, ao menos que escolha a opção para sair do programa
    match menu_opcao():
-      case 1:
-         match cooperativa():
-            case 1:
-                  dados_colunas=[]
-                  dados_linhas=[]
-                  dados_final=[]
-                  with open("cooperativa_log.txt","r") as arquivo: #leitura do arquivo e mostra o estoque detro dele
-                     conteudo= arquivo.read()
-                     dados_colunas= conteudo.split(";") #separar os dados em colunas, coloquei ";" pois estava dando conflito (com uma parte posterior do código) quando utilizava "\n"
-                     for linha in dados_colunas: #percorre a matriz "dados_colunas"
-                        dados_linhas= linha.split(",") #separar as colunas em linhas
-                        dados_final.append(dados_linhas)
-                  print(f'\nO estoque contém:')
-                  for linha in dados_final: #mostrar a matriz
-                     print(linha)
-            case 2:
-                  matriz=[]
-                  texto=""
-                  with open("cooperativa_log.txt","a") as arquivo: #leitura do arquivo e mostra o estoque detro dele
-                     Item=input("Digite o item a ser adicionado ao estoque: ")
-                     arquivo.write(f";{Item}") #adicionar o Item no arquivo
-                     while True: #validar o input
-                        try:
-                           Quantidade=int(input("Digite a quantidade do item a ser adicionado ao estoque: "))
-                        except:
-                           print("\nVocê digitou uma letra. Por favor digite apenas número(s).\n")
-                        else:
-                           arquivo.write(f",{Quantidade}") #adicionar a Quantidade no arquivo
-                           break
-                     # conteudo= arquivo.read()
-                     # for i in range(1):
-                     #       for j in range(1):
-                     #          addEstoque= input("digite o item a ser adicionado ao estoque: ")
-                     #          qtdEstoque= int(input("digite a quantidade do item a ser adicionada ao estoque: "))
-                     #          matriz.append(addEstoque)
-                     #          matriz.append(qtdEstoque)
-                              
-      case 4:
-         print("\nDesligando sistema. Obrigado por utilizá-lo ^^")
-         break
+         case 1:
+            while True:
+               match cooperativa():
+                  case 1:
+                        dados_colunas=[]
+                        dados_linhas=[]
+                        dados_final=[]
+                        with open("cooperativa_log.txt","r") as arquivo: #leitura do arquivo e mostra o estoque detro dele
+                           conteudo= arquivo.read()
+                           dados_colunas= conteudo.split(";") #separar os dados em colunas, coloquei ";" pois estava dando conflito (com uma parte posterior do código) quando utilizava "\n"
+                           for linha in dados_colunas: #percorre a matriz "dados_colunas"
+                              dados_linhas= linha.split(",") #separar as colunas em linhas
+                              dados_final.append(dados_linhas)
+                        print(f'\nO estoque contém:')
+                        for linha in dados_final: #mostrar a matriz
+                           print(linha)
+                  case 2:
+                        matriz=[]
+                        texto=""
+                        with open("cooperativa_log.txt","a") as arquivo: #leitura do arquivo e mostra o estoque detro dele
+                           Item=input("Digite o item a ser adicionado ao estoque: ")
+                           arquivo.write(f";{Item}") #adicionar o Item no arquivo
+                           while True: #validar o input
+                              try:
+                                 Quantidade=int(input("Digite a quantidade do item a ser adicionado ao estoque: "))
+                              except:
+                                 print("\nVocê digitou uma letra. Por favor digite apenas número(s).\n")
+                              else:
+                                 arquivo.write(f",{Quantidade}") #adicionar a Quantidade no arquivo
+                                 break
+                  case 4:
+                     print("\nDesligando sistema. Obrigado por utilizá-lo ^^")
+                     break
+                                 
+         case 4:
+            print("\nDesligando sistema. Obrigado por utilizá-lo ^^")
+            break
 
-      case _:
-         print("\nOpção inválida")
+         case _:
+            print("\nOpção inválida")
